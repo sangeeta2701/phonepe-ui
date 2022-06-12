84e9c22fb732d15d5866249791d7fde7eb25a2e1
DELL <anjugupta7060@gmail.com>
2022-06-12 15:33:32 +0530

    phonepe UI

--- lib/homePage/home.dart
+++ lib/homePage/home.dart
@@ -17,83 +17,86 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromRGBO(240, 226, 255, 1.0),
-      appBar: AppBar(
-        backgroundColor: Color.fromRGBO(119, 12, 143, 1.0),
-        leading: Padding(
-          padding: const EdgeInsets.all(8),
-          child: Container(
-            height: 60,
-            width: 50,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(12),
-              color: Colors.white,
-            ),
-            child: Icon(
-              Icons.person_outline,
-              color: Colors.purple,
-              size: 28,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(70),
+        child: AppBar(
+          backgroundColor: Color.fromRGBO(119, 12, 143, 1.0),
+          leading: Padding(
+            padding: const EdgeInsets.all(8),
+            child: Container(
+              height: 60,
+              width: 50,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                color: Colors.white,
+              ),
+              child: Icon(
+                Icons.person_outline,
+                color: Colors.purple,
+                size: 28,
+              ),
             ),
           ),
-        ),
-        title: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(
-                top: 15,
-              ),
-              child: Row(
-                children: [
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      "Add Addr...",
-                      style: TextStyle(color: Colors.white, fontSize: 18),
+          title: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 15,
+                ),
+                child: Row(
+                  children: [
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "Add Addr...",
+                        style: TextStyle(color: Colors.white, fontSize: 18),
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    width: 5,
-                  ),
-                  Icon(
-                    Icons.arrow_drop_down,
-                    color: Colors.white,
-                  )
-                ],
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Icon(
+                      Icons.arrow_drop_down,
+                      color: Colors.white,
+                    )
+                  ],
+                ),
               ),
+              TextField(
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                    hintText: 'Enter city name',
+                    hintStyle: TextStyle(color: Colors.white)),
+              ),
+            ],
+          ),
+          actions: [
+            Icon(
+              Icons.qr_code_scanner,
+              color: Colors.white,
+              size: 28,
             ),
-            TextField(
-              style: TextStyle(color: Colors.white),
-              decoration: InputDecoration(
-                  hintText: 'Enter city name',
-                  hintStyle: TextStyle(color: Colors.white)),
+            SizedBox(
+              width: 8,
+            ),
+            Icon(
+              Icons.notifications,
+              color: Colors.white,
+              size: 28,
+            ),
+            SizedBox(
+              width: 8,
+            ),
+            Icon(
+              Icons.help_outline,
+              color: Colors.white,
+              size: 30,
+            ),
+            SizedBox(
+              width: 8,
             ),
           ],
         ),
-        actions: [
-          Icon(
-            Icons.qr_code_scanner,
-            color: Colors.white,
-            size: 28,
-          ),
-          SizedBox(
-            width: 8,
-          ),
-          Icon(
-            Icons.notifications,
-            color: Colors.white,
-            size: 28,
-          ),
-          SizedBox(
-            width: 8,
-          ),
-          Icon(
-            Icons.help_outline,
-            color: Colors.white,
-            size: 30,
-          ),
-          SizedBox(
-            width: 8,
-          ),
-        ],
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -110,10 +113,38 @@ class _HomeScreenState extends State<HomeScreen> {
               padding: const EdgeInsets.all(8.0),
               child: RechargeBillContainer(),
             ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(),
           ],
         ),
       ),
       bottomNavigationBar: bottomNavigation(),
+      // bottomNavigationBar: BottomNavigationBar(
+      //   type: BottomNavigationBarType.fixed,
+      //   items: [
+      //     BottomNavigationBarItem(
+      //       label: "Home",
+      //       icon: Icon(Icons.home_filled),
+      //     ),
+      //     BottomNavigationBarItem(
+      //       label: "Store",
+      //       icon: Icon(Icons.home_filled),
+      //     ),
+      //     BottomNavigationBarItem(
+      //       label: "Insurance",
+      //       icon: Icon(Icons.home_filled),
+      //     ),
+      //     BottomNavigationBarItem(
+      //       label: "Wealth",
+      //       icon: Icon(Icons.home_filled),
+      //     ),
+      //     BottomNavigationBarItem(
+      //       label: "History",
+      //       icon: Icon(Icons.home_filled),
+      //     )
+      //   ],
     );
   }
 }
--- lib/homePage/recharge_bill_container.dart
+++ lib/homePage/recharge_bill_container.dart
@@ -16,6 +16,8 @@ class RechargeBillContainer extends StatelessWidget {
         borderRadius: BorderRadius.circular(16),
       ),
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           //recharge text
           Padding(
@@ -34,23 +36,21 @@ class RechargeBillContainer extends StatelessWidget {
           //1st row
           Padding(
             padding: const EdgeInsets.only(top: 10.0),
-            child: firstRow(),
+            child: firstColumn(),
           ),
-          SizedBox(
-            height: 10,
-          ),
-          //2nd row
-          secondRow(),
+          // SizedBox(
+          //   height: 10,
+          // ),
+          // //2nd row
+          // secondRow(),
         ],
       ),
     );
   }
 }
 
-class secondRow extends StatelessWidget {
-  const secondRow({
-    Key? key,
-  }) : super(key: key);
+class firstColumn extends StatelessWidget {
+  const firstColumn({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -59,168 +59,195 @@ class secondRow extends StatelessWidget {
       children: [
         Column(
           children: [
-            Icon(
-              FontAwesomeIcons.houseCircleCheck,
-              color: Color.fromRGBO(119, 12, 143, 1.0),
-              size: 35,
+            Column(
+              children: [
+                Icon(
+                  Icons.mobile_friendly,
+                  color: Color.fromRGBO(119, 12, 143, 1.0),
+                  size: 45,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "Mobile\nRecharge",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ],
             ),
             SizedBox(
               height: 10,
             ),
-            Text(
-              "Rent\nPayment",
-              style: TextStyle(
-                color: Colors.black,
-              ),
+            Column(
+              children: [
+                Icon(
+                  FontAwesomeIcons.houseCircleCheck,
+                  color: Color.fromRGBO(119, 12, 143, 1.0),
+                  size: 35,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "Rent\nPayment",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ],
             ),
           ],
         ),
-        Column(
-          children: [
-            Icon(
-              FontAwesomeIcons.sackDollar,
-              color: Color.fromRGBO(119, 12, 143, 1.0),
-              size: 35,
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              "Loan\nRepayment",
-              style: TextStyle(
-                color: Colors.black,
-              ),
-            ),
-          ],
-        ),
-        Column(
-          children: [
-            Icon(
-              Icons.propane_tank_outlined,
-              color: Color.fromRGBO(119, 12, 143, 1.0),
-              size: 35,
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              "Book A\n Cylinder",
-              style: TextStyle(
-                color: Colors.black,
-              ),
-            ),
-          ],
-        ),
-        Column(
-          children: [
-            Container(
-              height: 50,
-              width: 50,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                color: Color.fromRGBO(119, 12, 143, 1.0),
-              ),
-              child: Icon(
-                Icons.arrow_forward_ios_rounded,
-                color: Colors.white,
-                size: 35,
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              "See All",
-              style: TextStyle(
-                color: Colors.black,
-              ),
-            ),
-          ],
-        ),
-      ],
-    );
-  }
-}
 
-class firstRow extends StatelessWidget {
-  const firstRow({
-    Key? key,
-  }) : super(key: key);
+        //2nd column
 
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
         Column(
           children: [
-            Icon(
-              Icons.mobile_friendly,
-              color: Color.fromRGBO(119, 12, 143, 1.0),
-              size: 35,
+            Column(
+              children: [
+                Icon(
+                  FontAwesomeIcons.satelliteDish,
+                  color: Color.fromRGBO(119, 12, 143, 1.0),
+                  size: 45,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "DTH",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ],
             ),
             SizedBox(
-              height: 10,
+              height: 20,
             ),
-            Text(
-              "Mobile\nRecharge",
-              style: TextStyle(
-                color: Colors.black,
-              ),
+            Column(
+              children: [
+                Icon(
+                  FontAwesomeIcons.sackDollar,
+                  color: Color.fromRGBO(119, 12, 143, 1.0),
+                  size: 40,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "Loan\nRepayment",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+
+        //column3
+
+        Column(
+          children: [
+            Column(
+              children: [
+                Icon(
+                  Icons.lightbulb_outlined,
+                  color: Color.fromRGBO(119, 12, 143, 1.0),
+                  size: 45,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "Electricity",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Column(
+              children: [
+                Icon(
+                  Icons.propane_tank_outlined,
+                  color: Color.fromRGBO(119, 12, 143, 1.0),
+                  size: 45,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "Book A\nCylinder",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ],
             ),
           ],
         ),
         Column(
           children: [
-            Icon(
-              FontAwesomeIcons.satelliteDish,
-              color: Color.fromRGBO(119, 12, 143, 1.0),
-              size: 35,
+            Column(
+              children: [
+                Icon(
+                  Icons.credit_card,
+                  color: Color.fromRGBO(119, 12, 143, 1.0),
+                  size: 45,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "Credit Card\nBill Payment",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ],
             ),
             SizedBox(
               height: 10,
             ),
-            Text(
-              "DTH",
-              style: TextStyle(
-                color: Colors.black,
-              ),
-            ),
-          ],
-        ),
-        Column(
-          children: [
-            Icon(
-              Icons.lightbulb_outlined,
-              color: Color.fromRGBO(119, 12, 143, 1.0),
-              size: 35,
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              "Electricity",
-              style: TextStyle(
-                color: Colors.black,
-              ),
-            ),
-          ],
-        ),
-        Column(
-          children: [
-            Icon(
-              Icons.credit_card,
-              color: Color.fromRGBO(119, 12, 143, 1.0),
-              size: 35,
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              "Credit Card\nBill Payment",
-              style: TextStyle(
-                color: Colors.black,
-              ),
+            Column(
+              children: [
+                Container(
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: Color.fromRGBO(119, 12, 143, 1.0),
+                  ),
+                  child: Icon(
+                    Icons.arrow_forward_ios_rounded,
+                    color: Colors.white,
+                    size: 30,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "See All",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ],
             ),
           ],
         ),
